fe45a5972e6b6161f7e2390d1d725ef8cb67d783
Anandhu_Asokan <anandhuasokan888@gmail.com>
2026-03-06 13:13:46 +0530

    fix

--- src/app/environment/environment.prod.ts
+++ src/app/environment/environment.prod.ts
@@ -2,8 +2,9 @@ export const environment = {
     production: true,
 
     whatsappNumber:'8113847643',
-    emailServiceID: 'service_5zzwn1k',
-    emailTemplateID: 'template_4gesrev',
-    emailUserID: 'FjfYajRR1_Udl77Tn',
+    emailServiceID: 'service_x4qwx45',
+    emailTemplateID: 'template_7zn4jx9',
+    // emailUserID: 'FjfYajRR1_Udl77Tn',
+    emailUserID: 'ZWKXprpkd7h7no1fJ',
     replyToEmail: 'operations.rahmathulla@gmail.com'
   };
--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
     production: true,
 
-    whatsappNumber:'9562361118',
-    emailServiceID: 'service_5zzwn1k',
-    emailTemplateID: 'template_4gesrev',
-    emailUserID: 'FjfYajRR1_Udl77Tn',
+    whatsappNumber:'8113847643',
+    emailServiceID: 'service_x4qwx45',
+    emailTemplateID: 'template_7zn4jx9',
+    // emailUserID: 'FjfYajRR1_Udl77Tn',
+    emailUserID: 'ZWKXprpkd7h7no1fJ',
     replyToEmail: 'operations.rahmathulla@gmail.com'
   };
- 
