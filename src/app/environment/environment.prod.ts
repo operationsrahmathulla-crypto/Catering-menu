4e79a95efec098461a1ee989fa8e1ec30fdd8026
Anandhu_Asokan <anandhuasokan888@gmail.com>
2026-03-06 13:23:45 +0530

    fix

--- src/app/environment/environment.prod.ts
+++ src/app/environment/environment.prod.ts
@@ -4,7 +4,6 @@ export const environment = {
     whatsappNumber:'8113847643',
     emailServiceID: 'service_x4qwx45',
     emailTemplateID: 'template_7zn4jx9',
-    // emailUserID: 'FjfYajRR1_Udl77Tn',
     emailUserID: 'ZWKXprpkd7h7no1fJ',
     replyToEmail: 'operations.rahmathulla@gmail.com'
   };
